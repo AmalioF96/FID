186e618ca84e5009b8de56bd8fe8c2a6694f2faf
AmalioF96 <amaliocabeza.16@gmail.com>
2021-12-29 19:37:13 +0100

    Renombramos las columnas Mileage y Power

--- Preprocesado/main.r
+++ Preprocesado/main.r
@@ -11,7 +11,7 @@ library("stringi")
 # Importamos los archivos de preprocesado de las columnas
 source("Name.r")
 source("Kms_driven.r")
-source("Mileage.r")
+source("Consumption.r")
 
 #Carga de Datos
 # Lee fichero datasets/dataset.csv
@@ -19,14 +19,20 @@ datos <- read.csv("../Datos/bikes.csv", sep=",")
 # Mostramos los datos originales
 head(datos)
 
+print("")
 #########################
 # ETAPA DE PREPROCESADO #
 #########################
+
+#Renombramos las columnas de los datos originales
+names(datos)[names(datos) == 'mileage'] <- 'consumption'
+names(datos)[names(datos) == 'power'] <- 'BHP'
+
 datos = preprocesarColumnaName(datos);
-datos = preprocesarColumnaMileage(datos);
+datos = preprocesarColumnaConsumption(datos);
 datos = preprocesarColumnaKmsDriven(datos);
 
-print("")
+print("");
 
 # Mostrmos el resultado
 head(datos)
